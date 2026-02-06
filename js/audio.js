26fea0c9b8d57fcdc4f2b5e32b642584d5b54acd
shitcodersanonymous <shitcodersanonymous@users.noreply.github.com>
2026-02-05 21:24:23 -0500

    Increase delay to 4s

--- js/audio.js
+++ js/audio.js
@@ -25,7 +25,7 @@ window.AUDIO = {
           }).catch(function(e) {
             console.log('Music play failed:', e);
           });
-        }, 2500);
+        }, 4000);
       });
 
       this.bgMusic.load();
